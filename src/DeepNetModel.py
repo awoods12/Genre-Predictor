feaf30cd4a0cdba443570b87a3df753485dca4df
awoods12 <awoods12@terpmail.umd.edu>
2018-07-08 21:13:49 -0400

    added model save

--- src/DeepNetModel.py
+++ src/DeepNetModel.py
@@ -3,34 +3,39 @@ from PIL import Image
 import numpy as np
 from os import listdir
 import os
+import uuid
 
 def get_images(dir, type):
     files = listdir(dir)
-    im_arr = np.array(Image.open(dir + '\\' + files[0]).getdata())
-    if type == 'rap':
-        lab_arr = np.array([1, 0])
-    else:
-        lab_arr = np.array([0, 1])
-
-    for f in files[1:]:
-        image = np.array(Image.open(dir + '\\' + f).getdata())
-        im_arr = np.vstack([im_arr, np.atleast_2d(image)])
+    if len(files) > 0:
+        im_arr = np.array(Image.open(dir + '\\' + files[0]).getdata())
         if type == 'rap':
-            lab_arr = np.vstack([lab_arr, np.array([[1, 0]])])
+            lab_arr = np.array([1, 0])
         else:
-            lab_arr = np.vstack([lab_arr, np.array([[0, 1]])])
+            lab_arr = np.array([0, 1])
 
-    return im_arr, lab_arr
+        for f in files[1:]:
+            image = np.array(Image.open(dir + '\\' + f).getdata())
+            im_arr = np.vstack([im_arr, np.atleast_2d(image)])
+            if type == 'rap':
+                lab_arr = np.vstack([lab_arr, np.array([[1, 0]])])
+            else:
+                lab_arr = np.vstack([lab_arr, np.array([[0, 1]])])
+
+        return im_arr, lab_arr
+    return [],[]
 
 
 def png_input_fn(rap_dir,classical_dir):
     rap_im_arr,rap_lab_arr = get_images(rap_dir, 'rap')
-    classical_im_arr,classical_lab_arr = get_images(classical_dir, 'classical')
+    if rap_im_arr != []:
+        classical_im_arr,classical_lab_arr = get_images(classical_dir, 'classical')
 
-    im_arr = np.vstack([rap_im_arr,classical_im_arr])
-    lab_arr = np.vstack([rap_lab_arr, classical_lab_arr])
+        im_arr = np.vstack([rap_im_arr,classical_im_arr])
+        lab_arr = np.vstack([rap_lab_arr, classical_lab_arr])
 
-    return im_arr, lab_arr
+        return im_arr, lab_arr
+    return [],[]
 
 
 def get_model(input_shape):
@@ -74,21 +79,24 @@ accum_for_avg = 0
 # this loop gets the model and trains then evaluates the predictions 'runs' times
 for x in range(runs):
     input = png_input_fn(rap_dir=rap_dir,classical_dir=classical_dir)
-    model = get_model((128*128,))
-    train_runs = 5
+    if input[0] != []:
+        model = get_model((128*128,))
+        train_runs = 5
 
-    train_model(input, model, train_runs)
+        train_model(input, model, train_runs)
 
-    rap_eval_png_list = os.listdir(rap_eval_dir)
-    classical_eval_png_list = os.listdir(classical_eval_dir)
-    num_eval_files = len(rap_eval_png_list) + len(classical_eval_png_list)
-    eval_input = png_input_fn(rap_dir=rap_eval_dir,classical_dir=classical_eval_dir)
-    predictions = model.predict(x=eval_input[0],batch_size=1)
+        rap_eval_png_list = os.listdir(rap_eval_dir)
+        classical_eval_png_list = os.listdir(classical_eval_dir)
+        num_eval_files = len(rap_eval_png_list) + len(classical_eval_png_list)
+        eval_input = png_input_fn(rap_dir=rap_eval_dir,classical_dir=classical_eval_dir)
+        predictions = model.predict(x=eval_input[0],batch_size=1)
 
-    percentage = evaluate_predictions(eval_input, model, num_eval_files)
+        percentage = evaluate_predictions(eval_input, model, num_eval_files)
 
-    print(percentage)
-    accum_for_avg = accum_for_avg + percentage
+        print(percentage)
+        accum_for_avg = accum_for_avg + percentage
+
+        model.save('../models/model_' + str(uuid.uuid4()) + '.h5')
 
 # print average prediction accuracy for all of the training & prediction runs
 print(accum_for_avg/runs)
